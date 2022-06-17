092759af41415d73912914a2b552a9cd343eb1a9
eunchong2lee <tlfqjrjs1234@naver.com>
2022-06-17 17:38:44 +0800

    로그인 기능 만듬

---
+++ src/models/user.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose")
+// https://mongoosejs.com/docs/validation.html
+
+//https://runebook.dev/ko/docs/mongoose/schematypes
+const userschema = new Schema({
+    useremail: {
+        type: String,
+        required: [true, 'email을 입력하세요'],
+        unique: true,
+        trim: true,
+        
+    },
+    nickname: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        required: [true, 'nickname을 입력하세요'],
+        minlength: [3, '3글자 이상이어야 합니다.'],
+        maxlength: [18, '18글자 이하여야 합니다.'],
+        match: [/^[A-Za-z0-9]+$/, '특수문자가 포함되면 안됩니다.']
+
+    },
+    password: {
+        type: String,
+        required: true,
+        // enum: { values: [this.checkPassword], message: '비밀번호가 일치하지 않습니다.' }
+    },  
+    refreshToken : {
+        type : String,
+    }
+});
+
+const Users = model('user', userschema);
+module.exports = { Users, userschema }
---
+++ src/routes/userroutes.js
@@ -0,0 +1,93 @@
+// dotenv
+const dotenv = require('dotenv');
+dotenv.config()
+const jwtSecret = process.env.JWT-ACCESSKEY;
+const refreshjwtSecret = process.env.JWT-REFRESHKEY;
+
+const express = require("express")
+const userRouter = express.Router();
+
+const Bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { Users } = require("../models/");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+// passport config
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local');
+
+
+userRouter.post("/user", async (req, res) => {
+    try {
+        // 미들웨어에서 유저 정보 넣기 ( user_name, userId, password, checkPassword )
+        const { useremail, password, checkPassword, nickname} = req.body;
+        const checkUser = await Users.findOne().or([{ useremail }, { nickname }]);
+
+        if (password !== checkPassword) return res.status(400).json({ success: false, errorMessage: "비밀번호가 비밀번호 확인란과 동일하지 않습니다." });
+        if (checkUser) return res.status(400).json({ success: false, errorMessage: "이미 존재하는 아이디 또는 닉네임입니다." });
+
+        //hash 비밀번호
+        // 비밀 번호  hash / bcrypt
+
+        const salt = await Bcrypt.genSalt(Number(process.env.SALT-KEY));
+        const hashPassword = await Bcrypt.hash(password, salt);
+
+        const user = new Users({ useremail, password: hashPassword,nickname, refreshToken: null, });
+        await user.save();
+
+        return res.status(200).json({ success: true, message: "회원가입 했습니다.", user });
+  
+    } catch (err) {
+        const [errormessage] = Object
+            .values(err.errors)
+            .map((error) => error.message);
+        return res.status(400).json({ success: false, errorMessage: err.message });
+
+    }
+
+
+})
+
+userRouter.post("/login", async (req, res) => {
+    try {
+        const { useremail, password } = req.body;
+
+        // https://www.npmjs.com/package/bcrypt
+        const checkUser = await Users.findOne({ useremail });
+        const validPassword = await Bcrypt.compare(password, checkUser.password);
+        // true false
+
+        if (!checkUser) return res.status(400).json({ success: false, errorMessage: "아이디 또는 비밀번호가 틀렸습니다." });
+        if (!validPassword) return res.status(400).json({ success: false, errorMessage: " 아이디 또는 비밀번호가 틀렸습니다." });
+
+
+        // return res.status(400).json({ success: false, errorMessage: " 아이디 또는 비밀번호가 틀렸습니다." });
+        // 1시간 짜리 Token 만들기
+        const accesstoken = jwt.sign({ useremail }, jwtSecret, { expiresIn: '15s' });
+        const refreshToken = jwt.sign({}, refreshjwtSecret , {expiresIn: '7d'});
+        const user = await Users.findOneAndUpdate({useremail},{refreshToken}, {new:true});
+        //https://www.npmjs.com/package/jsonwebtoken 
+        console.log(accesstoken);
+        // dotenv 적용
+
+        res.status(200).json({ Message: "로그인 되었습니다.", accesstoken, user });
+
+    } catch (err) {
+        return res.status(400).send({ errorMeesage: err.message }) 
+    }
+
+
+});
+
+// 로컬 변수 데이터 들어오는 지 확인
+userRouter.get('/user/me', authMiddleware, async (req, res) => {
+    const { localuser } = res.locals;
+    res.json({
+        localuser,
+    });
+});
+
+
+
+module.exports = userRouter;    
