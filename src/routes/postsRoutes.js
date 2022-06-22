eead1bd453399a05a8eb7826bc74af8be476c03d
lojy4231 <lojy4231@gmail.com>
2022-06-23 01:21:44 +0900

    게시글 GET 요청에 쿼리스트링 추가

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,43 +1,46 @@
-const express = require("express")
-const postRouter = express.Router()
+const express = require("express");
+const qs = require("querystring");
 
-const {Posts} = require('../models/post')
+const postRouter = express.Router();
+
+const { Posts } = require('../models/post');
 
 
-postRouter.get('/posts', async (req, res)=>{
-    // #swagger.tags = ["Post"]
-    // #swagger.summary = "게시글 조회 페이지"
-    // #swagger.description = "게시글 조회 페이지"
+postRouter.get('/posts', async (req, res) => {
+  // #swagger.tags = ["Post"]
+  // #swagger.summary = "게시글 조회 페이지"
+  // #swagger.description = "게시글 조회 페이지"
   try {
-    const posts = await Posts.find();
+    const category = req.query.category;
+    if (!category) {
+      const posts = await Posts.find();
+      return res.status(200).json({ success: true, message: "게시글들을 불러왔습니다.", posts });
+    }
+    const posts = await Posts.find({ category });
+    return res.status(200).json({ success: true, message: "카테고리에 게시글들을 불러왔습니다.", posts });
+
+  } catch (error) {
+    return res.status(500).send({ errorMeesage: error.message })
+  }
+});
+
+postRouter.get('/posts/:postId', async (req, res) => {
+  // #swagger.tags = ["Post"]
+  // #swagger.summary = "게시글 상세 조회 페이지"
+  // #swagger.description = "게시글 상세 조회 페이지"
+  try {
+    const { postId } = req.params
+    const posts = await Posts.find({ postId })
     return res.status(200).json({
-      success:true,
-      message: "게시글들을 불러왔습니다.",
+      success: true,
+      message: "게시글을 불러왔습니다.",
       posts
     });
 
   } catch (error) {
-    return res.status(400).send({ errorMeesage: error.message }) 
-}
-})
-
-postRouter.get('/posts/:postId', async (req, res)=>{
-  // #swagger.tags = ["Post"]
-  // #swagger.summary = "게시글 상세 조회 페이지"
-  // #swagger.description = "게시글 상세 조회 페이지"
-try {
-  const { postId } = req.params
-  const posts = await Posts.find({ postId })
-  return res.status(200).json({
-    success:true,
-    message: "게시글을 불러왔습니다.",
-    posts
-  });
-
-} catch (error) {
-  return res.status(400).send({ errorMeesage: error.message }) 
-}
+    return res.status(400).send({ errorMeesage: error.message })
+  }
 })
 
 
-module.exports = {postRouter}
+module.exports = { postRouter }
